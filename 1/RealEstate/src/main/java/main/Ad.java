8a55202669b63d44e09cde2a0b5abfacc2c31ce0
hajos8 <rabb.andras@gmail.com>
2025-11-12 08:08:02 +0100

    Add ad

--- 1/RealEstate/src/main/java/main/Ad.java
+++ 1/RealEstate/src/main/java/main/Ad.java
@@ -1,4 +1,99 @@
 package main;
 
+import java.time.LocalDateTime;
+
 public class Ad {
+    int id;
+    String description;
+    int rooms;
+    int area;
+    int floors;
+    Category category;
+    Seller seller;
+    boolean freeOfCharge;
+    String imageUrl;
+    String latlong;
+    LocalDateTime createdAt;
+
+    public int getId() {
+        return id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getRooms() {
+        return rooms;
+    }
+    public int getArea() {
+        return area;
+    }
+    public int getFloors() {
+        return floors;
+    }
+    public Category getCategory() {
+        return category;
+    }
+    public Seller getSeller() {
+        return seller;
+    }
+    public boolean isFreeOfCharge() {
+        return freeOfCharge;
+    }
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    public String getLatlong() {
+        return latlong;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setRooms(int rooms) {
+        this.rooms = rooms;
+    }
+    public void setArea(int area) {
+        this.area = area;
+    }
+    public void setFloors(int floors) {
+        this.floors = floors;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+    public void setFreeOfCharge(boolean freeOfCharge) {
+        this.freeOfCharge = freeOfCharge;
+    }
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+    public void setLatlong(String latlong) {
+        this.latlong = latlong;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+    public Ad(int id, String description, int rooms, int area, int floors, Category category, Seller seller, boolean freeOfCharge, String imageUrl, String latlong, LocalDateTime createdAt) {
+        this.id = id;
+        this.description = description;
+        this.rooms = rooms;
+        this.area = area;
+        this.floors = floors;
+        this.category = category;
+        this.seller = seller;
+        this.freeOfCharge = freeOfCharge;
+        this.imageUrl = imageUrl;
+        this.latlong = latlong;
+        this.createdAt = createdAt;
+    }
 }
