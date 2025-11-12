3f2e93d6b78d5a5d382c66c88f4863cb71d40b16
hajos8 <rabb.andras@gmail.com>
2025-11-12 08:03:33 +0100

    Created project

---
+++ 1/RealEstate/src/main/java/main/Category.java
@@ -0,0 +1,25 @@
+package main;
+
+public class Category {
+    int id;
+    String name;
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
